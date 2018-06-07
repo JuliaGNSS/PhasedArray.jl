cbb4b630477c14d54acd9dd0190e960fb3fde11f
Sören Marcus Zorn <soeren.zorn@rwth-aachen.de>
2018-06-07 14:41:00 +0200

    Added @inferred tests

--- test/manifold.jl
+++ test/manifold.jl
@@ -7,13 +7,13 @@ using PhasedArray, StaticArrays, CoordinateTransformations
     c₀ = 299_792_458
     λ = c₀ / f_0
     ant_pos = λ / 4 * [1 -1 1 -1; 1 1 -1 -1; 0 0 0 0];
-    steer_vec = PhasedArray.manifold(ant_pos, f_0)
+    steer_vec = @inferred PhasedArray.manifold(ant_pos, f_0)
 
-    @test steer_vec(SVector(0,0,1)) ≈ [1, 1, 1, 1]
+    @test @inferred(steer_vec(SVector(0,0,1))) ≈ [1, 1, 1, 1]
 
-    @test steer_vec(SVector(0,1,0)) ≈ [1im, 1im, -1im, -1im]
+    @test @inferred(steer_vec(SVector(0,1,0))) ≈ [1im, 1im, -1im, -1im]
 
-    @test steer_vec(SVector(1,0,0)) ≈ [1im, -1im, 1im, -1im]
+    @test @inferred(steer_vec(SVector(1,0,0))) ≈ [1im, -1im, 1im, -1im]
   end
 
   @testset "LUT manifold" begin
@@ -25,15 +25,15 @@ using PhasedArray, StaticArrays, CoordinateTransformations
     antenna_gain = 2;
     sph2cart = CartesianFromSpherical()
     lut = [antenna_gain * cis(2 * π / λ * sph2cart(Spherical(1.0, θ * π / 180, π / 2 - ϕ * π / 180))' * ant_pos[:,ant]) for ant = 1:num_ants, ϕ=0:90, θ=0:359]
-    steer_vec = PhasedArray.manifold(lut, Interpolations.Constant(), π / 2)
+    steer_vec = @inferred PhasedArray.manifold(lut, Interpolations.Constant(), π / 2)
 
-    @test norm(steer_vec(Spherical(SVector(1, 1, 1)))) ≈ sqrt(num_ants)
+    @test norm(@inferred(steer_vec(Spherical(SVector(1, 1, 1))))) ≈ sqrt(num_ants)
 
-    @test steer_vec(Spherical(SVector(0,0,1))) ≈ [1, 1, 1, 1]
+    @test @inferred(steer_vec(Spherical(SVector(0,0,1)))) ≈ [1, 1, 1, 1]
 
-    @test steer_vec(Spherical(SVector(0,1,0))) ≈ [1im, 1im, -1im, -1im]
+    @test @inferred(steer_vec(Spherical(SVector(0,1,0)))) ≈ [1im, 1im, -1im, -1im]
 
-    @test steer_vec(Spherical(SVector(1,0,0))) ≈ [1im, -1im, 1im, -1im]
+    @test @inferred(steer_vec(Spherical(SVector(1,0,0)))) ≈ [1im, -1im, 1im, -1im]
   end
 
   @testset "LUT manifold interpolated" begin
@@ -45,21 +45,21 @@ using PhasedArray, StaticArrays, CoordinateTransformations
     antenna_gain = 2;
     sph2cart = CartesianFromSpherical()
     lut = [antenna_gain * cis(2 * π / λ * sph2cart(Spherical(1.0, θ * π / 180, π / 2 - ϕ * π / 180))' * ant_pos[:,ant]) for ant = 1:num_ants, ϕ=0:90, θ=0:359]
-    steer_vec = PhasedArray.manifold(lut, Interpolations.Linear(), π / 2)
+    steer_vec = @inferred PhasedArray.manifold(lut, Interpolations.Linear(), π / 2)
 
-    @test isapprox(norm(steer_vec(Spherical(SVector(1, 1, 1)))), sqrt(num_ants); rtol = 1e-4)
+    @test isapprox(norm(@inferred(steer_vec(Spherical(SVector(1, 1, 1))))), sqrt(num_ants); rtol = 1e-4)
 
-    @test steer_vec(Spherical(SVector(0,0,1))) ≈ [1, 1, 1, 1]
+    @test @inferred(steer_vec(Spherical(SVector(0,0,1)))) ≈ [1, 1, 1, 1]
 
-    @test steer_vec(Spherical(SVector(0,1,0))) ≈ [1im, 1im, -1im, -1im]
+    @test @inferred(steer_vec(Spherical(SVector(0,1,0)))) ≈ [1im, 1im, -1im, -1im]
 
-    @test steer_vec(Spherical(SVector(1,0,0))) ≈ [1im, -1im, 1im, -1im]
+    @test @inferred(steer_vec(Spherical(SVector(1,0,0)))) ≈ [1im, -1im, 1im, -1im]
   end
 
   @testset "Example LUT manifold" begin
     num_ants = size(EXAMPLE_LUT, 1)
-    steer_vec = PhasedArray.manifold(EXAMPLE_LUT)
-    @test norm(steer_vec(Spherical(SVector(0,0,1)))) < sqrt(num_ants)
+    steer_vec = @inferred PhasedArray.manifold(EXAMPLE_LUT)
+    @test norm(@inferred(steer_vec(Spherical(SVector(0,0,1))))) < sqrt(num_ants)
   end
 
 end
