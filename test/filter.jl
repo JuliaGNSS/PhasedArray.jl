d750f3540ccd2212e07fcfb0d901693f22671757
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-06-26 11:35:26 +0200

    adjust atol; because randn influences the error margin

--- test/filter.jl
+++ test/filter.jl
@@ -1,6 +1,6 @@
 @testset "Pre Whitening filter" begin
     test_signal = complex.(randn(2,1000), randn(2,1000)) / sqrt(2)
     pre_whitening_filter = PhasedArray.calc_whitening_filter(test_signal)
-    @test pre_whitening_filter ≈ eye(2) atol = 0.03
+    @test pre_whitening_filter ≈ eye(2) atol = 0.1
     @test filter(test_signal, eye(2)) ==  test_signal
 end
