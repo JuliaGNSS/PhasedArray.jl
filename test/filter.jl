d7588149adb98f6ea5242142d3770044514feee7
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-09-13 14:58:01 +0200

    adjust filter test to new input signal

--- test/filter.jl
+++ test/filter.jl
@@ -1,5 +1,5 @@
 @testset "Pre Whitening filter" begin
-    test_signal = complex.(randn(2,1000), randn(2,1000)) / sqrt(2)
+    test_signal = complex.(randn(1000,2), randn(1000,2)) / sqrt(2)
     pre_whitening_filter = PhasedArray.calc_whitening_filter(test_signal)
     @test pre_whitening_filter ≈ eye(2) atol = 0.1
     @test filter(eye(2), test_signal) ==  test_signal
