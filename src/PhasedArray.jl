7ef422905e52ded967e8700499994e216d1f6d2f
Aaron Leonardo Blume <aaron.blume@rwth-aachen.de>
2018-09-14 10:37:43 +0200

    rename whitening filter; add amplitude filter

--- src/PhasedArray.jl
+++ src/PhasedArray.jl
@@ -3,7 +3,7 @@ module PhasedArray
     using Interpolations, CoordinateTransformations, PyCall, PyPlot
     import Base.transpose, Base.filter
 
-    export manifold, pattern_plotting_data, plot_pattern, pattern_3D_plotting_data, plot_pattern_3D, plot_manifold_3D, draw_polar_axes, init_animate_pattern_data, animate_pattern, filter, calc_whitening_filter
+    export manifold, pattern_plotting_data, plot_pattern, pattern_3D_plotting_data, plot_pattern_3D, plot_manifold_3D, draw_polar_axes, init_animate_pattern_data, animate_pattern, filter, calc_prewhitening_filter, calc_amplitude_filter
 
     include("manifold.jl")
     include("plots.jl")
