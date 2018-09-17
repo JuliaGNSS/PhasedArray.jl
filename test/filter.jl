2410276739ceb667e2752c28a629020d61fce0e1
Sören Marcus Zorn <soeren.zorn@rwth-aachen.de>
2018-09-17 15:47:08 +0200

    Correct test for prewhitening filter and add test for amplitude filter

--- test/filter.jl
+++ test/filter.jl
@@ -1,6 +1,12 @@
 @testset "Pre Whitening filter" begin
     test_signal = complex.(randn(1000,2), randn(1000,2)) / sqrt(2)
-    pre_whitening_filter = PhasedArray.calc_whitening_filter(test_signal)
+    pre_whitening_filter = PhasedArray.calc_prewhitening_filter(test_signal)
     @test pre_whitening_filter ≈ eye(2) atol = 0.1
-    @test filter(eye(2), test_signal) ==  test_signal
+    @test filter(eye(2), test_signal) == test_signal
+end
+
+@testset "Amplitude filter" begin
+    test_signal = complex.(randn(1000,2), randn(1000,2)) / sqrt(2)
+    amplitude_filter = PhasedArray.calc_amplitude_filter(test_signal)
+    @test amplitude_filter ≈ eye(2) atol = 0.1
 end
