0c1ca7560ffcea066555536b12ab5823e7ef0a5c
Sören Marcus Zorn <soeren.zorn@rwth-aachen.de>
2018-09-28 13:46:45 +0200

    Correct test for filter based on variance / covariance

--- test/filter.jl
+++ test/filter.jl
@@ -1,10 +1,12 @@
 @testset "Pre Whitening filter" begin
     test_signal = complex.(randn(1000, 2), randn(1000, 2)) / sqrt(2)
-    pre_whitening_filter = calc_prewhitening_filter(test_signal)
+    Rxx = calc_variance_covariance(test_signal)
+    pre_whitening_filter = calc_prewhitening_filter(Rxx)
     @test pre_whitening_filter ≈ I atol = 0.1
 
     test_signal = complex.(randn(1000, 2), randn(1000, 2)) / sqrt(2) * sqrt(100) # Power: 100
-    pre_whitening_filter = calc_prewhitening_filter(test_signal)
+    Rxx = calc_variance_covariance(test_signal)
+    pre_whitening_filter = calc_prewhitening_filter(Rxx)
     @test pre_whitening_filter ≈ I atol = 0.1
 
     @test filter(Matrix{Complex{Float64}}(I, 2, 2), test_signal) == test_signal
@@ -12,16 +14,19 @@ end
 
 @testset "Amplitude filter" begin
     test_signal = complex.(randn(1000, 2), randn(1000, 2)) / sqrt(2)
-    amplitude_filter = calc_amplitude_filter(test_signal)
+    Rxx = calc_variance_covariance(test_signal)
+    amplitude_filter = calc_amplitude_filter(Rxx)
     @test amplitude_filter ≈ I atol = 0.1
 
     test_signal = complex.(randn(1000, 2), randn(1000, 2)) / sqrt(2) * sqrt(100) # Power: 100
-    pre_whitening_filter = calc_amplitude_filter(test_signal)
+    Rxx = calc_variance_covariance(test_signal)
+    pre_whitening_filter = calc_amplitude_filter(Rxx)
     @test pre_whitening_filter ≈ I atol = 0.1
 end
 
 @testset "Eigen beamformer" begin
     test_signal = complex.(randn(1000, 4), randn(1000, 4)) / sqrt(2) / 5 .+ ones(1000, 4)
-    beamformer = calc_eigen_beamformer(test_signal)
+    Rxx = calc_variance_covariance(test_signal)
+    beamformer = calc_eigen_beamformer(Rxx)
     @test beamformer ≈ -1 * [0.5, 0.5, 0.5, 0.5] atol = 0.03
 end
