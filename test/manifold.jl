c89f6587c5fdbccbe4554cf5355c9e7deb1f7b7a
Sören Marcus Zorn <soeren.zorn@rwth-aachen.de>
2019-04-02 15:10:44 +0200

    Increase expansion based on interpolation type

--- test/manifold.jl
+++ test/manifold.jl
@@ -1,6 +1,4 @@
-@testset "Antenna" begin
-
-  @testset "Ideal manifold" begin
+@testset "Ideal manifold" begin
     f_0 = 1575420e3
     c₀ = 299_792_458
     λ = c₀ / f_0
@@ -8,33 +6,26 @@
     steer_vec = @inferred PhasedArray.manifold(ant_pos, f_0)
 
     @test @inferred(steer_vec(SVector(0,0,1))) ≈ [1, 1, 1, 1]
-
     @test @inferred(steer_vec(SVector(0,1,0))) ≈ [1im, 1im, -1im, -1im]
-
     @test @inferred(steer_vec(SVector(1,0,0))) ≈ [1im, -1im, 1im, -1im]
-  end
+end
 
-  @testset "LUT manifold" begin
+@testset "LUT manifold" begin
     λ = 0.1904
     ant_pos = λ / 4 * [1 -1 1 -1; 1 1 -1 -1; 0 0 0 0];
     num_ants = size(ant_pos, 2)
-
     # Simulated measured pattern
     antenna_gain = 2;
     sph2cart = CartesianFromSpherical()
     lut = [antenna_gain * cis(2 * π / λ * sph2cart(Spherical(1.0, θ * π / 180, π / 2 - ϕ * π / 180))' * ant_pos[:,ant]) for ant = 1:num_ants, ϕ=0:90, θ=0:359]
     steer_vec = @inferred PhasedArray.manifold(lut, Interpolations.Constant(), π / 2)
-
     @test norm(@inferred(steer_vec(SphericalFromCartesian()(SVector(1, 1, 1))))) ≈ sqrt(num_ants)
-
     @test @inferred(steer_vec(SphericalFromCartesian()(SVector(0,0,1)))) ≈ [1, 1, 1, 1]
-
     @test @inferred(steer_vec(SphericalFromCartesian()(SVector(0,1,0)))) ≈ [1im, 1im, -1im, -1im]
-
     @test @inferred(steer_vec(SphericalFromCartesian()(SVector(1,0,0)))) ≈ [1im, -1im, 1im, -1im]
-  end
+end
 
-  @testset "LUT manifold interpolated" begin
+@testset "LUT manifold interpolated" begin
     λ = 0.1904
     ant_pos = λ / 4 * [1 -1 1 -1; 1 1 -1 -1; 0 0 0 0];
     num_ants = size(ant_pos, 2)
@@ -46,18 +37,32 @@
     steer_vec = @inferred PhasedArray.manifold(lut, Interpolations.Linear(), π / 2)
 
     @test isapprox(norm(@inferred(steer_vec(SphericalFromCartesian()(SVector(1, 1, 1))))), sqrt(num_ants); rtol = 1e-4)
-
     @test @inferred(steer_vec(SphericalFromCartesian()(SVector(0,0,1)))) ≈ [1, 1, 1, 1]
-
     @test @inferred(steer_vec(SphericalFromCartesian()(SVector(0,1,0)))) ≈ [1im, 1im, -1im, -1im]
-
     @test @inferred(steer_vec(SphericalFromCartesian()(SVector(1,0,0)))) ≈ [1im, -1im, 1im, -1im]
-  end
+end
 
-  @testset "Example LUT manifold" begin
+@testset "Example LUT manifold" begin
     num_ants = size(EXAMPLE_LUT, 1)
     steer_vec = @inferred PhasedArray.manifold(EXAMPLE_LUT)
     @test norm(@inferred(steer_vec(SphericalFromCartesian()(SVector(0,0,1))))) < sqrt(num_ants)
-  end
+end
 
+@testset "LUT expansion" begin
+    lut = randn(4, 10, 10)
+    lut_expanded = PhasedArray.expand(lut, 2)
+    @test size(lut_expanded) == (4, 14, 14)
+    @test lut_expanded[:,:,1] == lut_expanded[:,:,11]
+    @test lut_expanded[:,:,2] == lut_expanded[:,:,12]
+    @test lut_expanded[:,:,3] == lut_expanded[:,:,13]
+    @test lut_expanded[:,:,4] == lut_expanded[:,:,14]
+    @test lut_expanded[:,1,:] == lut_expanded[:,5,:]
+    @test lut_expanded[:,2,:] == lut_expanded[:,4,:]
+    @test lut_expanded[:,14,:] == lut_expanded[:,10,:]
+    @test lut_expanded[:,13,:] == lut_expanded[:,11,:]
+
+    @test PhasedArray.calc_expansion_length(Constant()) == 1
+    @test PhasedArray.calc_expansion_length(Constant()) == 1
+    @test PhasedArray.calc_expansion_length(Quadratic(Reflect(OnCell()))) == 2
+    @test PhasedArray.calc_expansion_length(Cubic(Line(OnGrid()))) == 4
 end
