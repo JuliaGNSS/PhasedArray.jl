3e992bb4d4900658b6c0ca1cb90186946095a42f
Michael Niestroj <michael.niestroj@rwth-aachen.de>
2021-06-10 09:25:34 +0000

    Implemented MUSIC based DOA estimation

--- test/pattern.jl
+++ test/pattern.jl
@@ -1,8 +1,14 @@
 @testset "Pattern" begin
     manifold = IdealManifold(1575420e3, 0.1904 / 4 * SVector(SVector(1, 1, 0), SVector(-1, 1, 0), SVector(1, -1, 0), SVector(-1, -1, 0)))
     pattern = Pattern(manifold, norm, num_az = 360, num_el = 91, max_el = π / 2)
-    @test pattern.azs == range(0, stop = 2π, length = 360)
-    @test pattern.els == range(0, stop = π / 2, length = 91)
+    @test first(pattern.azs) == 0.0
+    @test step(pattern.azs) == 2π/360
+    @test length(pattern.azs) == 360
+    @test isa(pattern.azs, AbstractRange)
+    @test first(pattern.els) == 0.0
+    @test step(pattern.els) == π/2 / 90
+    @test length(pattern.els) == 91
+    @test isa(pattern.els, AbstractRange)
     @test pattern.values ≈ ones(91, 360) * 2
     @test pattern.max_el == π / 2
 end
